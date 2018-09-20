9b328fe3b22e0a8bb36cb713dae0c584494cbe52
Oleksii Chumak <chumak.ac@gmail.com>
2018-09-20 13:33:04 +0300

    Added javadoc generation. Added javadoc

--- src/main/java/org/mset/MSet.java
+++ src/main/java/org/mset/MSet.java
@@ -48,7 +48,9 @@ public interface MSet<T> extends Iterable<T> {
      * Constructs a new set containing the elements in the specified
      * array.
      *
+     * @param <T>    the class of the objects in the array
      * @param values the array whose elements are to be placed into this set
+     * @return a set of the specified array
      * @throws NullPointerException if the specified array is null or contains a null
      */
     @SafeVarargs
@@ -60,45 +62,122 @@ public interface MSet<T> extends Iterable<T> {
      * Constructs a new set containing the elements in the specified
      * collection.
      *
+     * @param <T>   the class of the objects in the collection
      * @param value the collection whose elements are to be placed into this set
+     * @return a set of the specified collection
      * @throws NullPointerException if the specified collection is null or contains a null
      */
     static <T> MSet<T> of(Collection<T> value) {
         return new ValueMSet<>(value);
     }
 
+    /**
+     * Creates a new set that contains any element if it is present in at least one passed set.
+     *
+     * @param firstSet the first member of the union
+     * @param sets     the other members of the union
+     * @param <T>      the class of the objects in the sets
+     * @return firstSet &#8746; sets[0] &#8746; ... &#8746; sets[n]
+     */
     @SafeVarargs
-    static <E> MSet<E> union(MSet<E> firstSet, MSet<E>... sets) {
+    static <T> MSet<T> union(MSet<T> firstSet, MSet<T>... sets) {
         return new UnionMSet<>(MSetUtils.combineToList(firstSet, sets));
     }
 
+    /**
+     * Creates a new set that contains any element if it is present in all passed sets.
+     *
+     * @param firstSet the first member of the intersection
+     * @param sets     the other members of the intersection
+     * @param <T>      the class of the objects in the sets
+     * @return firstSet &#8745; sets[0] &#8745; ... &#8745; sets[n]
+     */
     @SafeVarargs
-    static <E> MSet<E> intersection(MSet<E> firstSet, MSet<E>... sets) {
+    static <T> MSet<T> intersection(MSet<T> firstSet, MSet<T>... sets) {
         return new IntersectionMSet<>(MSetUtils.combineToList(firstSet, sets));
     }
 
+    /**
+     * Returns a {@code Collector} that accumulates the input elements into a
+     * new {@code MSet}.
+     *
+     * @param <I> the type of the input elements
+     * @return a {@code Collector} which collects all the input elements into a
+     * {@code MSet}
+     */
     static <I> Collector<I, List<I>, MSet<I>> toMSet() {
         return new MSetCollector<>();
     }
 
+    /**
+     * Returns <tt>true</tt> if this set contains the specified element.
+     *
+     * @param value element whose presence in this set is to be tested
+     * @return <tt>true</tt> if this set contains the specified element
+     */
     boolean contains(Object value);
 
+    /**
+     * Returns <tt>true</tt> if this set contains the specified element.
+     *
+     * @param value        element whose presence in this set is to be tested
+     * @param universalSet a universal set shared with all sets involved in computation. Required if on some level
+     *                     a universal set bounded operation such as {@link #complement()} is used
+     * @return <tt>true</tt> if this set contains the specified element
+     */
     boolean contains(Object value, MSet<T> universalSet);
 
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @param universalSet a universal set shared with all sets involved in computation. Required if on some level
+     *                     a universal set bounded operation such as {@link #complement()} is used
+     * @return an Iterator.
+     */
     Iterator<T> iterator(MSet<T> universalSet);
 
+    /**
+     * Returns a proxy of the universal set bounded to the current context. If no set is present, any operation on the proxy will fail.
+     * The method is used internally by other sets.
+     *
+     * @return a proxy of the universal set bounded to current context
+     */
     MSet<T> getUniversalSet();
 
+    /**
+     * Returns a set that contains all subsets of the current set
+     *
+     * @return P(this)
+     */
     default MSet<MSet<T>> powerSet() {
         return new PowerMSet<>(this);
     }
 
+    /**
+     * Returns the number of elements in this set.
+     *
+     * @return | this |
+     */
     long cardinality();
 
+    /**
+     * Creates a new set that contains any element if it is present in the current set or in the passed set
+     *
+     * @param set the second member of the union
+     * @return this &#8746; set
+     * @see MSet#union(MSet, MSet[])
+     */
     default MSet<T> union(MSet<T> set) {
         return MSet.union(this, set);
     }
 
+    /**
+     * Creates a new set that contains any element if it is present in the current set and in the passed set
+     *
+     * @param set the second member of the intersection
+     * @return this &#8745; set
+     * @see MSet#intersection(MSet, MSet[])
+     */
     default MSet<T> intersection(MSet<T> set) {
         return MSet.intersection(this, set);
     }
@@ -128,6 +207,14 @@ public interface MSet<T> extends Iterable<T> {
         return isSubsetOf(set) && isSuperSetOf(set);
     }
 
+    /**
+     * Returns a sequential {@code Stream} with this set as its source.
+     *
+     * <p>The default implementation creates a sequential {@code Stream} from the
+     * sets's {@code Spliterator}.</p>
+     *
+     * @return a sequential {@code Stream} over the elements in this set
+     */
     default Stream<T> stream() {
         return StreamSupport.stream(spliterator(), false);
     }
