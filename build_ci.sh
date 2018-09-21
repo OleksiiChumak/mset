24ad1411ed75506d92e5735f3f1fda7f6240c999
Oleksii Chumak <chumak.ac@gmail.com>
2018-09-21 09:42:33 +0300

    updated .travis.yml

--- build_ci.sh
+++ build_ci.sh
@@ -6,10 +6,10 @@ if [[ $PUBSLISH_TO_MAVEN == 'true' &&  $TRAVIS_BRANCH  == 'master' ]]
 then
     echo Jars will be published to maven repository
     echo $SIGN_KEY | base64 -d > secring.gpg
-    BUILD_ARGS=$BUILD_ARGS uploadArchives -Psigning.keyId=$SING_KEY_ID -Psigning.password=$SIGN_PASSWORD -Psigning.secretKeyRingFile=secring.gpg -PossrhUsername=$OSSRH_USERNAME -PossrhPassword=$OSSRH_PASSWORD
+    BUILD_ARGS=$BUILD_ARGS 'uploadArchives -Psigning.keyId=$SING_KEY_ID -Psigning.password=$SIGN_PASSWORD -Psigning.secretKeyRingFile=secring.gpg -PossrhUsername=$OSSRH_USERNAME -PossrhPassword=$OSSRH_PASSWORD'
 else
     echo Set PUBSLISH_TO_MAVEN=true to publish jars. Publish works only on master brunch
-    BUILD_ARGS=$BUILD_ARGS -x singing
+    BUILD_ARGS=$BUILD_ARGS '-x signArchives'
 fi
 
 echo $BUILD_ARGS
