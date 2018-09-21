7e901558bbcc101b2a4eb7ca46b8eafa67156084
Oleksii Chumak <chumak.ac@gmail.com>
2018-09-21 10:10:57 +0300

    updated .travis.yml

--- build_ci.sh
+++ build_ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BUILD_ARGS='build -s --no-daemon jacocoTestReport sonarqube -Dsonar.projectKey=OleksiiChumak_mset -Dsonar.organization=oleksiichumak-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN'
+BUILD_ARGS="build -s --no-daemon jacocoTestReport sonarqube -Dsonar.projectKey=OleksiiChumak_mset -Dsonar.organization=oleksiichumak-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN"
 
 if [[ $PUBSLISH_TO_MAVEN == 'true' &&  $TRAVIS_BRANCH  == 'master' ]]
 then
@@ -8,12 +8,12 @@ then
     echo Jars will be published to maven repository
     echo
     echo $SIGN_KEY | base64 -d > secring.gpg
-    BUILD_ARGS+=' uploadArchives -Psigning.keyId=$SING_KEY_ID -Psigning.password=$SIGN_PASSWORD -Psigning.secretKeyRingFile=secring.gpg -PossrhUsername=$OSSRH_USERNAME -PossrhPassword=$OSSRH_PASSWORD'
+    BUILD_ARGS+=" uploadArchives -Psigning.keyId=$SING_KEY_ID -Psigning.password=$SIGN_PASSWORD -Psigning.secretKeyRingFile=secring.gpg -PossrhUsername=$OSSRH_USERNAME -PossrhPassword=$OSSRH_PASSWORD"
 else
     echo
     echo Set PUBSLISH_TO_MAVEN=true to publish jars. Publish works only on master brunch
     echo
-    BUILD_ARGS+=' -x signArchives'
+    BUILD_ARGS+=" -x signArchives"
 fi
 
 ./gradlew $BUILD_ARGS
